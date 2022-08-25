bd65006f630a7d0f65e9bb4f734dfc4f7b959ffa
xiaobebe <wyy.xb@qq.com>
2022-08-25 15:05:55 +0800

    update module dep

--- plugins/type-file/parse-cli.js
+++ plugins/type-file/parse-cli.js
@@ -1,13 +1,12 @@
-const { parse } = require("extract-typescript-types");
-const path = require("path");
-const mapValues = require("lodash/mapValues");
+import { parse } from "extract-typescript-types";
+import path from "path";
 
 const fileName = process.argv[2];
 
 const componentDoc = parse([path.resolve(process.cwd(), fileName)]).map(
   (item) => {
     let { block, mtime, method, props, ...rest } = item;
-    props = mapValues(props, ({ parent, ...rest }) => rest);
+    Object.values(props).forEach((item) => delete item.parent);
     return {
       props,
       ...rest,
