b382833033f5936bd02259a07c6b5eade9e05c81
xiaobebe <wyy.xb@qq.com>
2022-11-24 17:33:56 +0800

    fix: the style of antd

--- templates/common/build.mjs
+++ templates/common/build.mjs
@@ -1,10 +1,13 @@
 import esbuild from "esbuild";
 import { sassPlugin } from "esbuild-sass-plugin";
 
+const isProd = process.env.NODE_ENV === "production";
+
 esbuild
   .build({
     // esbuild ./src/index.ts --watch --bundle --outfile=dist/main.js --external:vscode --format=cjs --platform=node
-    watch: true,
+    minify: isProd,
+    watch: !isProd,
     bundle: true,
     outfile: "dist/index.js",
     entryPoints: ["src/index.tsx"],
