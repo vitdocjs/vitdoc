a2d436ae60727ad1c18dc4e72e863b8cb2c4d259
xiaobebe <wyy.xb@qq.com>
2022-11-26 15:53:56 +0800

    feat: add mobile template

---
+++ templates/mobile/src/index.tsx
@@ -0,0 +1,111 @@
+import { ProLayout } from "@ant-design/pro-layout";
+import { LinkCopy, useComponentInfo, useRouteMap } from "@vitdoc/ui";
+import { useBoolean } from "ahooks";
+import classNames from "classnames";
+import React from "react";
+import ReactDOM from "react-dom";
+import {
+  HashRouter,
+  Navigate,
+  Route,
+  Routes,
+  useLocation,
+  useNavigate,
+} from "react-router-dom";
+import ReadmePane from "./pages/readme-pane";
+
+import "./global.scss";
+
+export function App() {
+  const { tree: menuData, routes } = useRouteMap() || {};
+
+  menuData?.forEach((item) => (item.icon = <span>📦</span>));
+
+  const push = useNavigate();
+
+  const { npmLink } = useComponentInfo() || ({} as any);
+
+  const [collapsed, { toggle }] = useBoolean(false);
+
+  const { pathname } = useLocation();
+
+  if (!routes) {
+    return null;
+  }
+
+  return (
+    <ProLayout
+      siderMenuType="group"
+      title={false}
+      hide={routes.length < 2}
+      collapsed={collapsed}
+      onCollapse={toggle}
+      breakpoint="lg"
+      location={{ pathname }}
+      className={classNames(
+        "vitdoc-layout",
+        collapsed && "vitdoc-layout-collapsed"
+      )}
+      logo={
+        <a className="vitdoc-logo" href={npmLink}>
+          <img
+            alt="Vite Docs"
+            src={
+              // @ts-ignore
+              window.pageConfig?.logo ||
+              "//vitdocjs.github.io/logo-with-word.svg"
+            }
+            style={{ minHeight: 30 }}
+          />
+        </a>
+      }
+      route={{ routes: menuData }}
+      pageTitleRender={(props, defaultPageTitle) => {
+        return `${defaultPageTitle} - Vitdoc`;
+      }}
+      menu={{
+        defaultOpenAll: true,
+        hideMenuWhenCollapsed: true,
+        ignoreFlatMenu: true,
+      }}
+      menuItemRender={(item, dom, { collapsed }) => {
+        if (collapsed) {
+          return "";
+        }
+        return React.cloneElement(dom as any, {
+          onClick: () => push(item.path!),
+          children: <LinkCopy route={item.path}>{item.name}</LinkCopy>,
+        });
+      }}
+    >
+      <Routes>
+        {routes.map((route) => {
+          return (
+            <Route
+              path={route}
+              element={
+                <div style={{ display: "flex" }} className="code-box-demo">
+                  <ReadmePane key={`${route}_readme_pane`} />
+                </div>
+              }
+            />
+          );
+        })}
+        <Route path="*" element={<Navigate to={routes[0]} replace={true} />} />
+      </Routes>
+    </ProLayout>
+  );
+}
+
+export function mount({ container }) {
+  ReactDOM.render(
+    <HashRouter>
+      <App />
+    </HashRouter>,
+    container
+  );
+}
+
+export function unmount({ container }) {
+  ReactDOM.unmountComponentAtNode(container);
+}
---
+++ templates/mobile/src/pages/readme-pane/index.tsx
@@ -0,0 +1,76 @@
+import { PageContainer } from "@ant-design/pro-layout";
+import { useUnmount } from "ahooks";
+import { Typography } from "antd";
+import React from "react";
+import {
+  LinkCopy,
+  MarkdownArea,
+  PropertyPane,
+  Store,
+  useComponentInfo,
+  useMarkdown,
+  useRoute,
+  useRouteMap,
+} from "@vitdoc/ui";
+
+import "./index.scss";
+
+const { Text } = Typography;
+
+export default function ReadmePane() {
+  const { route } = useRoute();
+
+  const Components = useMarkdown();
+
+  const setProperties = Store.useSetProperties();
+  const setDefaultProps = Store.useSetDefaultProps();
+
+  useUnmount(() => {
+    setProperties({});
+    setDefaultProps({
+      current: undefined,
+      defaultProps: {},
+      props: {},
+    });
+  });
+
+  const compInfo = useComponentInfo();
+
+  const { flattenRoutes = [] } = useRouteMap() || {};
+
+  const currentRoute = flattenRoutes.find(({ path }) => path === route);
+
+  return (
+    <div id="public-component-show-container">
+      {Components ? (
+        <PageContainer
+          title={
+            <LinkCopy route={route}>
+              {currentRoute?.name || compInfo?.packageName}
+            </LinkCopy>
+          }
+          subTitle={
+            <a href={compInfo?.npmLink} className="vitdoc-link-title">
+              <Text type="secondary">Package: {compInfo?.packageName}</Text>
+              <Text type="secondary" style={{ marginLeft: 8 }}>
+                Version: {compInfo?.packageVersion}
+              </Text>
+            </a>
+          }
+          className="vitdoc-component-page"
+        >
+          <div className="component-main">
+            <div className="component-part">
+              <div className="component-description">
+                <MarkdownArea data={Components} />
+              </div>
+            </div>
+            <PropertyPane />
+          </div>
+        </PageContainer>
+      ) : (
+        "Loading..."
+      )}
+    </div>
+  );
+}
